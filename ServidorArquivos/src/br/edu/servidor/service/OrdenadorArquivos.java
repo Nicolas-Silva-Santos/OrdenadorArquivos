68b223cf0d25f5adb8220554575fcdb4818ce3fc
Nicolas-Silva-Santos <nicolas_silva-santos@estudante.sesisenai.org.br>
2026-06-09 19:44:43 -0300

    quick sort

--- ServidorArquivos/src/br/edu/servidor/service/OrdenadorArquivos.java
+++ ServidorArquivos/src/br/edu/servidor/service/OrdenadorArquivos.java
@@ -11,43 +11,61 @@ public class OrdenadorArquivos {
         if (lista == null || lista.size() <= 1) {
             return lista == null ? new ArrayList<>() : new ArrayList<>(lista);
         }
+
         ArquivoInfo[] arr = lista.toArray(new ArquivoInfo[0]);
-        mergesort(arr, 0, arr.length - 1, ascendente);
+
+        quickSort(arr, 0, arr.length - 1, ascendente);
+
         return new ArrayList<>(Arrays.asList(arr));
     }
 
-    private void mergesort(ArquivoInfo[] arr, int left, int right, boolean asc) {
-        if (left >= right) return;
-        int mid = left + (right - left) / 2;
-        mergesort(arr, left, mid, asc);
-        mergesort(arr, mid + 1, right, asc);
-        merge(arr, left, mid, right, asc);
+    private void quickSort(ArquivoInfo[] arr, int low, int high, boolean asc) {
+        if (low < high) {
+
+            int pivotIndex = partition(arr, low, high, asc);
+
+            quickSort(arr, low, pivotIndex - 1, asc);
+            quickSort(arr, pivotIndex + 1, high, asc);
+        }
     }
 
-    private void merge(ArquivoInfo[] arr, int left, int mid, int right, boolean asc) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        ArquivoInfo[] L = new ArquivoInfo[n1];
-        ArquivoInfo[] R = new ArquivoInfo[n2];
-        System.arraycopy(arr, left, L, 0, n1);
-        System.arraycopy(arr, mid + 1, R, 0, n2);
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            long a = L[i].getTamanho();
-            long b = R[j].getTamanho();
-            int cmp = Long.compare(a, b);
-            if (cmp == 0) {
-                // tie-breaker: use name to ensure deterministic order
-                cmp = L[i].getNome().compareTo(R[j].getNome());
-            }
-            boolean takeLeft = asc ? (cmp <= 0) : (cmp >= 0);
-            if (takeLeft) {
-                arr[k++] = L[i++];
-            } else {
-                arr[k++] = R[j++];
+    private int partition(ArquivoInfo[] arr, int low, int high, boolean asc) {
+
+        ArquivoInfo pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            int cmp = comparar(arr[j], pivot);
+
+            boolean deveTrocar = asc ? (cmp <= 0) : (cmp >= 0);
+
+            if (deveTrocar) {
+                i++;
+                trocar(arr, i, j);
             }
         }
-        while (i < n1) arr[k++] = L[i++];
-        while (j < n2) arr[k++] = R[j++];
+
+        trocar(arr, i + 1, high);
+
+        return i + 1;
+    }
+
+    private int comparar(ArquivoInfo a, ArquivoInfo b) {
+
+        int cmp = Long.compare(a.getTamanho(), b.getTamanho());
+
+        if (cmp == 0) {
+            cmp = a.getNome().compareTo(b.getNome());
+        }
+
+        return cmp;
+    }
+
+    private void trocar(ArquivoInfo[] arr, int i, int j) {
+        ArquivoInfo temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
